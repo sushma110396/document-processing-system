ad463e6de0e9af9d327c543711e2d998f32f4af6
Sushma Hegde <hegde.sus@northeastern.edu>
2025-02-28 18:31:40 -0800

    Adding project setup files

---
+++ document-processing/src/test/java/com/example/documentprocessing/DocumentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
