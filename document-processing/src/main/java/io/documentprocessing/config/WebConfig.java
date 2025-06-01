3f47ed474a663f9cc351d72bd81f47648b78a9a0
Sushma Hegde <hegde.sus@northeastern.edu>
2025-06-01 10:07:46 -0700

    Fix CORS: switched to allowedOriginPatterns

--- document-processing/src/main/java/io/documentprocessing/config/WebConfig.java
+++ document-processing/src/main/java/io/documentprocessing/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
+                .allowedOriginPatterns(
                                 "http://localhost:3000",
                                 "http://localhost:5173",
                                 "http://document-processing-system.s3-website-us-west-1.amazonaws.com",
