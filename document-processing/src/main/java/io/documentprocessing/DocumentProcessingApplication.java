448dc077bf4924204e09ed918f6cb7ea5e41f3cb
Sushma Hegde <hegde.sus@northeastern.edu>
2025-06-03 19:41:38 -0700

    Excluded UserDetailsServiceAutoConfiguration class

--- document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
+++ document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
@@ -2,12 +2,10 @@ package io.documentprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
-@SpringBootApplication(scanBasePackages = "io.documentprocessing", exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "io.documentprocessing",  exclude = {org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class})
 @EnableAsync
 public class DocumentProcessingApplication {
 
