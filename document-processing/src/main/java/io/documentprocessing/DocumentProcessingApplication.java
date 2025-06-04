7f5614935499f4c81446c3d1957e3d47bed8998f
Sushma Hegde <hegde.sus@northeastern.edu>
2025-06-03 17:30:18 -0700

    Added annotation to exclude security auto configuration

--- document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
+++ document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
@@ -2,9 +2,11 @@ package io.documentprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(scanBasePackages = "io.documentprocessing")
+
+@SpringBootApplication(scanBasePackages = "io.documentprocessing", exclude = {SecurityAutoConfiguration.class })
 @EnableAsync
 public class DocumentProcessingApplication {
 
