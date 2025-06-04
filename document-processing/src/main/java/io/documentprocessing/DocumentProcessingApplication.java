381484fc6175d1ae2fae82f9319a583fdd728845
Sushma Hegde <hegde.sus@northeastern.edu>
2025-06-03 18:43:24 -0700

    Added code to exclude auto configuration class

--- document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
+++ document-processing/src/main/java/io/documentprocessing/DocumentProcessingApplication.java
@@ -3,10 +3,11 @@ package io.documentprocessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
-@SpringBootApplication(scanBasePackages = "io.documentprocessing", exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(scanBasePackages = "io.documentprocessing", exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableAsync
 public class DocumentProcessingApplication {
 
